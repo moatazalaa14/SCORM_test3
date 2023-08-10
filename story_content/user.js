82e5bd871c6df51bf31999403b54a8bd5e48deed
moataz <mutazalaasalah@gmail.com>
2023-08-10 18:19:10 +0300

    first commit

---
+++ story_content/user.js
@@ -0,0 +1,183 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "5qcecJTguGs":
+        Script1();
+        break;
+      case "5yI04Rf03dm":
+        Script2();
+        break;
+      case "6MZYiJh6Jal":
+        Script3();
+        break;
+      case "60R6ap59Bdc":
+        Script4();
+        break;
+      case "610yPvju1IK":
+        Script5();
+        break;
+      case "6aqcpNJTi2W":
+        Script6();
+        break;
+      case "6cRL6SN4mZV":
+        Script7();
+        break;
+      case "6kdL7zRMULN":
+        Script8();
+        break;
+      case "6KJsH0H1bjv":
+        Script9();
+        break;
+      case "5eCCaxiJfmx":
+        Script10();
+        break;
+      case "6CKkG5b74FB":
+        Script11();
+        break;
+      case "6LgPdM4DpwN":
+        Script12();
+        break;
+      case "64NUXXlJhH1":
+        Script13();
+        break;
+      case "5x5gRPBcR7l":
+        Script14();
+        break;
+      case "5u8ywbFj05d":
+        Script15();
+        break;
+      case "5gGjdvSbNqe":
+        Script16();
+        break;
+  }
+}
+
+function Script1()
+{
+  document.querySelectorAll('audio, video').forEach(item => {
+  item.muted = false;
+  item.pause();
+});
+}
+
+function Script2()
+{
+  document.querySelectorAll('audio, video').forEach(item => {
+  item.muted = false;
+  item.play();
+});
+}
+
+function Script3()
+{
+  document.querySelectorAll('audio, video').forEach(item => {
+  item.muted = false;
+  item.pause();
+});
+}
+
+function Script4()
+{
+  document.querySelectorAll('audio, video').forEach(item => {
+  item.muted = false;
+  item.play();
+});
+}
+
+function Script5()
+{
+  document.querySelectorAll('audio, video').forEach(item => {
+  item.muted = false;
+  item.pause();
+});
+}
+
+function Script6()
+{
+  document.querySelectorAll('audio, video').forEach(item => {
+  item.muted = false;
+  item.play();
+});
+}
+
+function Script7()
+{
+  document.querySelectorAll('audio, video').forEach(item => {
+  item.muted = false;
+  item.pause();
+});
+}
+
+function Script8()
+{
+  document.querySelectorAll('audio, video').forEach(item => {
+  item.muted = false;
+  item.play();
+});
+}
+
+function Script9()
+{
+  document.querySelectorAll('audio, video').forEach(item => {
+  item.muted = false;
+  item.pause();
+});
+}
+
+function Script10()
+{
+  document.querySelectorAll('audio, video').forEach(item => {
+  item.muted = false;
+  item.play();
+});
+}
+
+function Script11()
+{
+  document.querySelectorAll('audio, video').forEach(item => {
+  item.muted = false;
+  item.pause();
+});
+}
+
+function Script12()
+{
+  document.querySelectorAll('audio, video').forEach(item => {
+  item.muted = false;
+  item.play();
+});
+}
+
+function Script13()
+{
+  document.querySelectorAll('audio, video').forEach(item => {
+  item.muted = false;
+  item.pause();
+});
+}
+
+function Script14()
+{
+  document.querySelectorAll('audio, video').forEach(item => {
+  item.muted = false;
+  item.play();
+});
+}
+
+function Script15()
+{
+  document.querySelectorAll('audio, video').forEach(item => {
+  item.muted = false;
+  item.pause();
+});
+}
+
+function Script16()
+{
+  document.querySelectorAll('audio, video').forEach(item => {
+  item.muted = false;
+  item.play();
+});
+}
+
